3138860f49ae8880f259cab9dab2e1d922f9049a
raj248 <shashankraj007281@gmail.com>
2025-11-15 22:20:15 +0530

    using pnpm and xlsx for client side parsing

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { uploadFile } from "@/services/fileApi";
+import * as XLSX from "xlsx";
 
 export default function FileUploader() {
   const [file, setFile] = useState<File | null>(null);
@@ -10,9 +11,30 @@ export default function FileUploader() {
     setStatus("Uploading...");
 
     try {
-      const result = await uploadFile(file, "user3@example.com");
-      console.log("Uploaded Data:", result);
+      // const result = await uploadFile(file, "user3@example.com");
+      // console.log("Uploaded Data:", result);
       setStatus("Upload successful ✅");
+
+      const reader = new FileReader();
+      reader.readAsArrayBuffer(file);
+      reader.onload = (e) => {
+        const csvData = e.target?.result;
+        console.log("CSV Content:", csvData);
+        console.log(file.type);
+
+        const workbook = XLSX.read(csvData, { type: "buffer" });
+        console.log(workbook);
+        const worksheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[worksheetName];
+        // const data = XLSX.utils.sheet_to_json(worksheet, { range: 3 });
+        const data = XLSX.utils.sheet_to_csv(worksheet);
+        // console.log("Headers: ", Object.keys(data[0] as {}));
+        console.log(typeof data);
+      };
+      // reader.readAsText(file);
+
+      // Reset the file input
+      // setFile(null);
     } catch (err) {
       console.error(err);
       setStatus("Upload failed ❌");
@@ -23,7 +45,8 @@ export default function FileUploader() {
     <div className="p-4 border rounded-md">
       <input
         type="file"
-        accept=".csv,.xml"
+        accept=".csv,.xml, .xlsx"
+        // className="hidden"
         onChange={(e) => setFile(e.target.files?.[0] || null)}
       />
       <button
